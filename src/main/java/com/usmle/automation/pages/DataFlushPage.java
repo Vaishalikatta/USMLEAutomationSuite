508abb226a2b967cbaf82c14097deeed57bac197
Brar, Ranjit <ranjit.brar@happiestminds.com>
2015-09-22 00:12:40 +0530

    updated

--- src/main/java/com/usmle/automation/pages/DataFlushPage.java
+++ src/main/java/com/usmle/automation/pages/DataFlushPage.java
@@ -15,14 +15,46 @@ public class DataFlushPage extends BasePage {
 
 	private Logger logger = LogManager.getLogger(this.getClass());
 
+	TextHandler textHandler = new TextHandler(driver);
 	LinkHandler linkHandler = new LinkHandler(driver);
 	DropDownHandler dropdownHandler = new DropDownHandler(driver);
-	TextHandler textHandler = new TextHandler(driver);
 
 	public DataFlushPage(WebDriver webDriver) {
 		super(webDriver);
 	}
 
+	/*
+	 * Opening of USMLE Dataflush Login page
+	 */
+	private void openUsmleDataFlushLoginPage() {
+		driver.get(PropertiesRepository.getString("usmle.dataflush.loginpage.url"));
+		setDriverWait(PropertiesRepository.getString("usmle.dataflush.button.login"));
+	}
+
+	/*
+	 * Login to USMLE Data Flush application
+	 */
+	public void loginToUsmleDataFlushApplicationPage() {
+
+		// Opening of USMLE Dataflush login page
+		openUsmleDataFlushLoginPage();
+		// Entering UserId and Password
+		try {
+			textHandler.writeText(PropertiesRepository.getString("usmle.dataflush.textfield.username"),
+					PropertiesRepository.getString("usmle.dataflush.textfield.value.username"));
+
+			textHandler.writeText(PropertiesRepository.getString("usmle.dataflush.textfield.password"),
+					PropertiesRepository.getString("usmle.dataflush.textfield.value.password"));
+		} catch (DriverException e) {
+			logger.error("Unable to enter username and/or password", e);
+		}
+		try {
+			linkHandler.selectLink(PropertiesRepository.getString("usmle.dataflush.button.login"),
+					PropertiesRepository.getString("usmle.dataflush.button.login.waitfor"));
+		} catch (DriverException e) {
+			logger.error("Unable to open USMLE DataFlush Login page", e);
+		}
+	}
 	/*
 	 * Flush USMLE Data
 	 */
@@ -49,5 +81,5 @@ public class DataFlushPage extends BasePage {
 		} catch (DriverException e) {
 			logger.error("Unable to open flush USMLE data", e);
 		}
-	}
+	}	
 }
